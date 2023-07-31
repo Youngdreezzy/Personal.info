11cb34c1232e3c1d75e648f3da94539338aa6cd8
Dreezzy <afolabiopesimon@gmail.com>
2023-07-31 22:49:37 +0100

    Personal.py

---
+++ Personal.py
@@ -0,0 +1,19 @@
+Name=input("Enter Your Full Name:")
+print("Hello", Name, "Welcome to the IT world, i beleive this will be a great stepstone for you")
+Age=input("Enter Your Age if Above 18:")
+State=input("Enter Your State of Origin:")
+Occupation=input("What do you do for a living?:")
+print("I hope all the information listed below is correct with the information you provided")
+print("Name:",Name)
+print("Age:", Age)
+print("State:", State)
+print("Occupation:", Occupation)
+outcome=input("Enter Yes or No if the information is Correct:")
+if outcome=="Yes":
+    print("Welcome To My World")
+else:
+    print("Kindly contact the Support for correction of intormation")
+
+print("Kindly navigate and explore through the website")
+print("This is a short note for you", Name)
+print("Hi,", Name, "I believe you are a cute and lovable Human full of love and care, Never stop being who you, keep grinding and trust in God.\nIt will all make sense at the end of the tunnel")
